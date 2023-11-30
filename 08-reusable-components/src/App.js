5d4c923535ff5dd317dcd4265dd66eb75212d20d
Artem Borodovoy <a.borodovoi@gmail.com>
2023-11-30 22:22:12 +0200

    reusable components

---
+++ 08-reusable-components/src/App.js
@@ -0,0 +1,18 @@
+import MyComponent from './components/MyComponent';
+import OtherComponent from './components/OtherComponent';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <OtherComponent />
+      <MyComponent />
+      <OtherComponent />
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
