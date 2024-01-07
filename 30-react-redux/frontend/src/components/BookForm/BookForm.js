1948c271c59303eb48cda6517510c6c64ca06784
Artem Borodovoy <a.borodovoi@gmail.com>
2024-01-07 21:09:18 +0200

    added book list

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,14 +1,21 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/actionCreators';
 import './BookForm.css';
 
 const BookForm = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title && author) {
-      // dispatch ation
+      const book = {
+        title,
+        author,
+      };
+      dispatch(addBook(book));
       setTitle('');
       setAuthor('');
     }
