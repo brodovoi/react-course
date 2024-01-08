18ff89c3a1308295b2290f7ce6bf108c5d755834
Artem Borodovoy <a.borodovoi@gmail.com>
2024-01-08 23:17:30 +0200

    clear filters

--- 30-react-redux/frontend/src/components/BookList/BookList.js
+++ 30-react-redux/frontend/src/components/BookList/BookList.js
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs';
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators';
+import { selectTitleFilter } from '../../redux/slices/filterSlice';
 import './BookList.css';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const titleFilter = useSelector(selectTitleFilter);
   const dispatch = useDispatch();
 
   const handleDeleteBook = (id) => {
@@ -15,6 +17,14 @@ const BookList = () => {
     dispatch(toggleFavorite(id));
   };
 
+  const filteredbBooks = books.filter((book) => {
+    const mathesTitle = book.title
+      .toLowerCase()
+      .includes(titleFilter.toLowerCase());
+
+    return mathesTitle;
+  });
+
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -22,7 +32,7 @@ const BookList = () => {
         <p>No books available</p>
       ) : (
         <ul>
-          {books.map((book, i) => (
+          {filteredbBooks.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
                 {++i} .{book.title} by <strong>{book.author}</strong>
