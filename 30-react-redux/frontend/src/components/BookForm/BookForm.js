7888995018b2b93d7abbb0d8430c04724f64c6bd
Artem Borodovoy <a.borodovoi@gmail.com>
2024-01-11 02:24:34 +0200

    added source

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -16,7 +16,7 @@ const BookForm = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length);
     const randomBook = booksData[randomIndex];
 
-    const randomBookWithId = createBookWithID(randomBook);
+    const randomBookWithId = createBookWithID(randomBook, 'random');
 
     dispatch(addBook(randomBookWithId));
   };
@@ -25,7 +25,7 @@ const BookForm = () => {
     e.preventDefault();
 
     if (title && author) {
-      const book = createBookWithID({ title, author });
+      const book = createBookWithID({ title, author }, 'manual');
       dispatch(addBook(book));
       setTitle('');
       setAuthor('');
@@ -37,7 +37,7 @@ const BookForm = () => {
       const res = await axios.get('http://localhost:4000/random-book');
 
       if (res?.data?.title && res?.data?.author) {
-        dispatch(addBook(createBookWithID(res.data)));
+        dispatch(addBook(createBookWithID(res.data, 'API')));
       }
     } catch (error) {
       console.log('Error Feetching random book', error);
